d2926e15842b6b493a0ad80c47a1c80992ca1678
reygptaa <95678485+reygptaa@users.noreply.github.com>
2024-07-11 20:38:18 +0530

    Add files via upload

---
+++ Q5/answer5.cpp
@@ -0,0 +1,8 @@
+rotate(vector<int>& nums, int k) 
+{
+        int l=nums.size();
+        k%=l;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());      
+}
